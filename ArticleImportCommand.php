23156f6c9a36afa0bd2b117c6bf8de5a1e2069a6
jailbeebs <247666973+jailbeebs@users.noreply.github.com>
2026-03-08 02:06:38 +0100

    Fix formatting of USED_BIKE_IMPORT_META_KEY constant

--- ArticleImportCommand.php
+++ ArticleImportCommand.php
@@ -4,7 +4,7 @@ class ArticleImportCommand
 {  
     private const MERCHANT_MOTORPROFI = 30001;
  
-    private const USED_BIKE_IMPORT_META_KEY = 'used_bike_import_enabled';
+    private const USED_BIKE_IMPORT_META_KEY = 'used_bike_import_enabled'; 
     private const USED_BIKE_FAMILY_CODES = ['ba_gebrauchtraeder'];
 
     private $importFormat = 'csv' ;
