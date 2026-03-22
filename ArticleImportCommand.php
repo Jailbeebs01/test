7bde3aec12a1114aec62d61e78bd70ea7758ddb9
jailbeebs <247666973+jailbeebs@users.noreply.github.com>
2026-03-22 19:32:34 +0100

    Fix formatting of importFormat variable declaration

--- ArticleImportCommand.php
+++ ArticleImportCommand.php
@@ -7,7 +7,7 @@ class ArticleImportCommand
     private const USED_BIKE_IMPORT_META_KEY = 'used_bike_import_enabled'; 
     private const USED_BIKE_FAMILY_CODES = ['ba_gebrauchtraeder'];
 
-    private $importFormat = 'csv' ;
+    private $importFormat = 'csv' ; 
     private $usedBikeImportPermissionByMerchant = [];
   
     private function isUsedBikeImportRow(array $article, int $merchantId, string $akeneoFamilyCode, \Pegasus_Plugins_Components_ArticleComponent $articleComponent): bool
